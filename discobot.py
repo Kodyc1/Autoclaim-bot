c35457d4406b13287f636ea47ccaae2ec9ff3557
Kodyc1 <Kodyc1@users.noreply.github.com>
2019-04-18 00:26:54 -0500

    Add and List for waifus work, but not remove

--- discobot.py
+++ discobot.py
@@ -133,10 +133,11 @@ async def on_message(message):
         pickle_out.close()
 
     ''' remove from waifulist '''
-    if message.author.id == '220296856800854018' and message.content.startswith('`remove'):
+    if message.author.id == '220296856800854018' and message.content.startswith('!!remove'):
         pickle_in = open("waifu.pickle", "rb")
         waifulist = pickle.load(pickle_in)
-        if message.content[7:].lower() in waifulist:
+        if (message.content[8:].lower()) in waifulist:
+            print('i should technically be removing message.content[8:]')
             waifulist.remove(message.content[7:])
         pickle_in.close()
         
