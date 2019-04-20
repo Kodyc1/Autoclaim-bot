f62ea129206acf27589fcb07ef689ccfb483ef89
Kodyc1 <Kodyc1@users.noreply.github.com>
2019-04-19 19:33:49 -0500

    created pickling helper functions, created ~set waifulist command, and refactored commands

--- discobot.py
+++ discobot.py
@@ -1,6 +1,7 @@
 from config import TOKEN
 from datetime import datetime
 from pathlib import Path
+import ast
 import asyncio
 import discord
 import logging 
@@ -12,7 +13,22 @@ import pickle
 
 # https://discordapp.com/api/oauth2/authorize?client_id=567157490765266944&permissions=523328&scope=bot
 
-''' list of waifus to claim ''' 
+# Channel IDs
+
+# testmudae 567154106528170006
+# testmudae.general 567154106528170012
+
+# eyedentity server 205903858604572672
+# waifu channel 564189777398726666
+# waifu-claimer 566856249937756161
+
+
+
+#################
+### CONSTANTS ###
+#################
+
+''' base list of waifus to claim ''' 
 waifus = ['akeno himejima', 'rias gremory', 'riven', 'ciri', 'shinoa hiragi',
           'ringo noyamano', 'mikuru asahina', 'aria shichijou', 'cynthia', 
           'skyla', 'chika fujiwara', 'jibril', 'pharah', 'blair', 'utaha kasumigaoka', 
@@ -28,11 +44,18 @@ users = ['220296856800854018', '200481198525513728', '567153956900569109']
 ''' INSTANTIATE CLIENT '''
 client = discord.Client()
 
-''' CAN I CLAIM???? global variable '''
+''' Claimable global variable '''
 claimable = True
 
-''' background task that just '''
+
+
+#######################
+### Background Loop ###
+#######################
+
 async def my_background_task():
+    ''' Background task that just resets claimable every 3 hours
+    '''
     await client.wait_until_ready()
 
     time = datetime.now()
@@ -47,15 +70,26 @@ async def my_background_task():
 @client.event
 async def on_ready():
     print('Logged on as', client.user)
+
+
+########################
+### Helper functions ###
+########################
     
-# Channel IDs
+def read_pickle(server_pickle, Guild):
+    pickle_in = open(Guild + '.pickle', "rb")
+    waifulist = pickle.load(pickle_in)
+    pickle_in.close()
+    return waifulist
 
-# testmudae 567154106528170006
-# testmudae.general 567154106528170012
+def write_pickle(server_pickle, Guild, data):
+    pickle_out = open(Guild + '.pickle', "wb")
+    pickle.dump(data, pickle_out)
+    pickle_out.close()
 
-# eyedentity server 205903858604572672
-# waifu channel 564189777398726666
-# waifu-claimer 566856249937756161
+#######################
+###    ON MESSAGE   ###
+#######################
 
 
 @client.event
@@ -67,104 +101,139 @@ async def on_message(message):
     global claimable
     global users
     global waifus
+
+    server_pickle = Path(message.Guild.name + '.pickle')
+
+
+    #########################
+    ### Claim and Logging ###
+    #########################
     
-    ''' if mudamaid 25 in this channel sees waifu in waifu list, claim '''
-    if message.author.name:# == 'Mudamaid 25':
+    if message.author.name:
         if (message.channel.id == '564189777398726666' or 
             message.channel.id == '567154106528170012' or 
             message.channel.id == '566856249937756161'):
            
             await asyncio.sleep(2)
             
-            print(("Channel: {} \n" +
+            print(("Server - Channel: {} - {} \n" +
                   "Message author disc id: {} \n" +
                   "Message content: {} \n" +
                   "Message type: {} \n" +
                   "Message reactions: {} \n" +
                   "Message embeds: {} \n" +
-                  "\n").format(message.channel,
-                                message.author,
-                                message.content,
-                                message.type,
-                                message.reactions,
-                                message.embeds))
+                  "\n").format(message.Guild,
+                               message.channel,
+                               message.author,
+                               message.content,
+                               message.type,
+                               message.reactions,
+                               message.embeds))
                         
             if claimable:
-                if message.embeds:
-                    if len(message.reactions) < 2:
-                        if message.reactions:
-                            if message.embeds[0]['author']['name'].lower() in waifus:
-                                #print('hello'+str(message.reactions[0].emoji)+'\n')
-                                await client.add_reaction(message, message.reactions[0].emoji)
-                                claimable = False
+                
+                if message.embeds and len(message.reactions) == 1:
+                    
+                    waifulist = read_pickle(server_pickle)
+                    
+                    if message.embeds[0]['author']['name'].lower() in waifulist:
 
-    ''' ping pong '''
+                        await client.add_reaction(message, message.reactions[0].emoji)
+                        
+                        claimable = False
+
+
+    ''' play ping pong '''
     if message.content == 'ping':
         await client.send_message(message.channel, content='pong')
-    ''' echo for trade '''
+        
+    ''' ~echo for trade '''
     if message.author.id in users and message.content.startswith("~echo"):
         await client.send_message(message.channel, message.content[5:])
 
-    ''' react to target message IDs '''
+    ''' ~react <target_message_ID> '''
     if message.author.id in users and message.content.startswith("~react"):
+
         target_message_id = message.content[6:]
+        
         target_message = await client.get_message(message.channel, target_message_id)
-        if target_message.embeds:
-            if target_message.reactions:
+
+        if target_message.embeds and target_message.reactions:
+
                 await client.add_reaction(target_message, target_message.reactions[0].emoji)
 
-    ''' display list '''
-    if ((message.author.id == '220296856800854018' or
-       message.author.id == '200481198525513728') and
-       message.content.startswith("~list")):
-        server_file = Path(message.Guild.id + '.pickle')
-        #if server_file.exists():
-        pickle_in = open("waifu.pickle", "rb")
-        waifulist = pickle.load(pickle_in)
-        pickle_in.close()
-        #await client.add_reaction(message, '\u2705')
-        await client.send_message(message.channel, content=str(waifulist))
 
-    ''' add to waifulist '''
-    if ((message.author.id == '220296856800854018' or
-       message.author.id == '200481198525513728') and
-       message.content.startswith("~add")):
-        server_file = Path(message.Guild.id + '.pickle')
-        #if server_file.exists():
-        pickle_in = open("waifu.pickle", "rb")
-        waifulist = pickle.load(pickle_in)
-        waifulist.append(message.content[4:].strip())
-        pickle_in.close()
+    ####################
+    ##### COMMANDS #####
+    ####################
+
+
+    ''' ~list to display wishlist '''
+    if (message.content == "~list") and (message.author.id in users):
+        # if pickle already exists, read from it and display it
+        if server_pickle.exists():            
+            waifulist = read_pickle(server_pickle, message.Guild.name)
+            await client.send_message(message.channel, content=str(waifulist))
+            
+        # else write the base list to a server.pickle and display it
+        else:
+            await client.send_message(message.channel, content="Creating base list")
+            write_pickle(server_pickle, message.Guild.name, waifus)
+            await client.send_message(message.channel, content=str(waifus))
+
+
+    ## beta
+    ''' ~set [] waifulist '''
+    if ((message.author.id in users) and message.content.startswith("~set")):
+        if server_file.exists():
+            waifulist = ast.literal_eval(message.content[4:].strip())
+
+            write_pickle(server_pickle, message.Guild.name, waifulist)
+
+            await client.add_reaction(message, '\u2705')
+            
+
+    ''' ~add <waifu>
+
+    adds a waifu to waifulist
+
+    '''
+    if ((message.author.id in users) and message.content.startswith("~add")):
+        if server_pickle.exists():
+            waifulist = read_pickle(server_pickle, message.Guild.name)
+
+            waifulist.append(message.content[4:].strip())
+
+            write_pickle(server_pickle, message.Guild.name, waifulist)
+            
+            await client.add_reaction(message, '\u2705')
+
+    ''' `remove <waifu>
+
+    removes a waifu from waifulist
+
+    '''
+    if ((message.author.id in users) and message.content.startswith('`remove')):
+        if server_pickle.exists():
+            waifulist = read_pickle(server_pickle, message.Guild.name)
         
-        pickle_out=open("waifu.pickle", "wb")
-        pickle.dump(waifulist, pickle_out)
-        pickle_out.close()
-        await client.add_reaction(message, '\u2705')
+            waifu = message.content[7:].lower().strip()
 
-    ''' remove from waifulist '''
-    if ((message.author.id == '220296856800854018' or
-       message.author.id == '200481198525513728') and
-       message.content.startswith('`remove')):
-        server_file = Path(message.Guild.id + '.pickle')
-        #if server_file.exists():
-        pickle_in = open("waifu.pickle", "rb")
-        waifulist = pickle.load(pickle_in)
-        waifu = message.content[7:].lower().strip()
-        print(waifu)
-        if (waifu) in waifulist:
-            waifulist.remove(waifu)
-        pickle_in.close()
-        
-        pickle_out=open("waifu.pickle", "wb")
-        pickle.dump(waifulist, pickle_out)
-        pickle_out.close()
-        await client.add_reaction(message, '\u2705')
+            if (waifu) in waifulist:
+                waifulist.remove(waifu)
 
-    ''' claimable? '''
-    if ((message.author.id in users) and message.content.startswith('~claim')):
-        # await client.add_reaction(message,'\u2705')
+            write_pickle(server_pickle, message.Guild.name, waifulist)
+
+            await client.add_reaction(message, '\u2705')
+
+
+    ''' ~claim  '''
+    ''' if claimable, return True, else return False '''
+    if ((message.author.id in users) and (message.content == '~claim')):
+        await client.add_reaction(message,'\u2705')
         if claimable:
             await client.send_message(message.channel, content=str(claimable))
+
 
 client.loop.create_task(my_background_task())
 
